f3ad65c704816d8ed831382d70b1cc2cbad5cd2e
Sean Jun <seanjun21@gmail.com>
2016-06-24 18:10:23 -0700

    Fully functional w/ league ranking, group stages, etc

--- js/api_hack.js
+++ js/api_hack.js
@@ -25,6 +25,7 @@ $(function() {
         event.preventDefault();
         getPlayer(teamHashMap[searchTerm]);
     });
+
 });
 
 // Loading website
@@ -59,22 +60,53 @@ function loadSite() {
 function getLeague(leagueID) {
     $.ajax({
         headers: { 'X-Auth-Token': 'e34ad8f9aebb436eb3437851ca9b581a' },
-        url: 'http://api.football-data.org/v1/soccerseasons/' + leagueID + '/teams',
+        url: 'http://api.football-data.org/v1/soccerseasons/' + leagueID + '/leagueTable',
         dataType: 'json',
         type: 'GET',
     }).done(function(response) {
-        $('.results').html('');
+        if (response.leagueCaption !== "Champions League 2015/16") {
+            $('.results').html('<table class="table" id="league-result"></table>');
+            $('.league #league-category').clone().appendTo('#league-result');
+            $.each(response.standing, function(key, value) {
+                // Retrieved url from API --> Retrived numbers from url --> Sliced front to get teamID
 
-        teamHashMap = {};
-        $.each(response.teams, function(key, value) {
-            // Retrieved url from API --> Retrived numbers from url --> Sliced front to get teamID
-            var hrefTeams = value._links.fixtures.href;
-            var teamID = hrefTeams.match(/\d/g).join("").slice(1);
-            teamHashMap[value.name] = teamID;
+                var hrefTeams = value._links.team.href;
+                var teamID = hrefTeams.match(/\d/g).join("").slice(1);
+                teamHashMap[value.teamName] = teamID;
 
-            var leagueShown = showLeague(value.name);
-            $('.results').append(leagueShown);
-        });
+                var leagueShown = showLeague(value.position, value.crestURI, value.teamName, value.playedGames, value.wins, value.draws, value.losses, value.goals, value.points);
+
+                $('.results #league-result').append(leagueShown);
+            });
+        } else {
+
+            // Clear our results and append new table set for Champs
+            $('.results').html('');
+            // Run each method over A ~ H (8 Groups)
+            $.each(response.standings, function(key, value) {
+                $('.results').append('<table class="table" id="champs-result-' + key + '"></table>');
+                $('.champs #champs-category').clone().appendTo('#champs-result-' + key);
+
+                console.log(value, 'VAUE');
+
+
+
+                $.each(value, function(index, value) {
+
+                    var teamID = value.teamId;
+                    teamHashMap[value.team] = teamID;
+                    var champsShown = showChamps(value.group, value.rank, value.crestURI, value.team, value.playedGames, value.goals, value.points);
+                    $('.results #champs-result-' + key).append(champsShown);
+
+                });
+            });
+            console.log(teamHashMap, 'teamHashMap');
+            // Run each method again over 4 teams
+
+            // Set variable ChampsShown and use it to store outcome of showChamps function
+
+            // Append the outcome to new table
+        }
     });
 }
 
@@ -101,7 +133,7 @@ function getPlayer(teamID) {
         type: 'GET',
     }).done(function(response) {
         $('.results .extra-container').html('<table class="table" id="players-result"></table>');
-        $('.players #category').clone().appendTo('#players-result');
+        $('.players #players-category').clone().appendTo('#players-result');
         $.each(response.players, function(key, value) {
             var playerShown = showPlayer(value.name, value.position, value.jerseyNumber, value.nationality, value.marketValue);
 
@@ -110,10 +142,84 @@ function getPlayer(teamID) {
     });
 }
 
+// Display champs info on HTML
+function showChamps(groups, rank, crest, name, games, goals, points) {
+    // Clone template
+    var copy = $('.template #champs-values ').clone();
+
+    // Set champs group
+    var champsGroup = copy.find('.champs-group');
+    champsGroup.text(groups);
+
+    // Set champs rank
+    var champsRank = copy.find('.champs-rank');
+    champsRank.text(rank);
+
+    // Set champs crest
+    var champsCrest = copy.find('.champs-crest img');
+    champsCrest.attr('src', crest);
+
+    // Set champs name
+    var champsName = copy.find('.champs-name');
+    champsName.html('<div class="clubs"><a>' + name + '</a></div>');
+
+    // Set champs games
+    var champsGames = copy.find('.champs-games');
+    champsGames.text(games);
+
+    // Set champs goals
+    var champsGoals = copy.find('.champs-goals');
+    champsGoals.text(goals);
+
+    // Set champs points
+    var champsPoints = copy.find('.champs-points');
+    champsPoints.text(points);
+
+    return copy;
+}
+
 // Display league info on HTML
-function showLeague(league) {
-    var link = '<div><a class="clubs">' + league + '</a></div>';
-    return link;
+function showLeague(rank, crest, name, games, wins, draws, losses, goals, points) {
+    // Clone template
+    var copy = $('.template #league-values ').clone();
+
+    // Set league rank
+    var leagueRank = copy.find('.league-rank');
+    leagueRank.text(rank);
+
+    // Set league crest
+    var leagueCrest = copy.find('.league-crest img');
+    leagueCrest.attr('src', crest);
+
+    // Set league name
+    var leagueName = copy.find('.league-name');
+    leagueName.html('<div class="clubs"><a>' + name + '</a></div>');
+
+    // Set league games
+    var leagueGames = copy.find('.league-games');
+    leagueGames.text(games);
+
+    // Set league wins
+    var leagueWins = copy.find('.league-wins');
+    leagueWins.text(wins);
+
+    // Set league draws
+    var leagueDraws = copy.find('.league-draws');
+    leagueDraws.text(draws);
+
+    // Set league losses
+    var leagueLosses = copy.find('.league-losses');
+    leagueLosses.text(losses);
+
+    // Set league goals
+    var leagueGoals = copy.find('.league-goals');
+    leagueGoals.text(goals);
+
+    // Set league points
+    var leaguePoints = copy.find('.league-points');
+    leaguePoints.text(points);
+
+    return copy;
 }
 
 // Display team info on HTML
@@ -139,7 +245,7 @@ function showTeam(team) {
 
 function showPlayer(player, position, number, nationality, value) {
     // Clone template
-    var copy = $('.template #values ').clone();
+    var copy = $('.template #players-values ').clone();
 
     // Set player name
     var playerName = copy.find('.players-name');
